51f3c2db2d70c489c5e715ca307331099ac6bcab
liuxiaoyu-git <xiaoyliu@redhat.com>
2019-01-10 11:16:25 +0800

    Update HomeController.java

--- spring/demo-spring-boot/src/main/java/com/mastertheboss/HomeController.java
+++ spring/demo-spring-boot/src/main/java/com/mastertheboss/HomeController.java
@@ -27,6 +27,6 @@ public class HomeController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String ipaddress() throws Exception {
-        return "Reply: " + welcome;
+        return "Reply: " + Hello World;
     }
 }
